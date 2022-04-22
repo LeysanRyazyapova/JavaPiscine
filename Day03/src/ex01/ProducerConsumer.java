522ffa89c00f341040a078cf2e360437a426b736
tmiranda <tmiranda@student.21-school.ru>
2022-04-22 08:52:39 +0300

    1

---
+++ Day03/src/ex01/ProducerConsumer.java
@@ -0,0 +1,43 @@
+package ex01;
+
+public class ProducerConsumer {
+
+    private int count;
+
+    private Boolean flag = true;
+
+    private final Object lock = new Object();
+
+    public ProducerConsumer(int count) {
+        this.count = count;
+    }
+
+    public void produce() throws InterruptedException {
+
+        for (int i = 0; i < count; i++) {
+            synchronized (lock) {
+                if(flag == false) {
+                    lock.wait();
+                }
+                System.out.println("Egg");
+                flag = false;
+                lock.notify();
+            }
+        }
+    }
+
+    public void consume() throws InterruptedException {
+
+        for (int i = 0; i < count; i++) {
+            synchronized (lock) {
+                if(flag == true) {
+                    lock.wait();
+                }
+                System.out.println("Hen");
+                flag = true;
+                lock.notify();
+            }
+        }
+    }
+
+}
